94261bc77aa5a2552f10f774223ca11aa80e0bee
michik4 <mihalixz1221@gmail.com>
2026-09-22 04:21:28 +0300

    fix: minor fixes

--- apps/web-desktop/src/app/page.tsx
+++ apps/web-desktop/src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Test } from "@repo/ui/components/Test";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-
+      <Test />
     </div>
   );
 }
